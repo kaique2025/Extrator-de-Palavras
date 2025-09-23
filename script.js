c2383adbcbfa23cb8af434c7cd3e28fbe1902dde
kaique2025 <kaique.muniz.lopes@escola.pr.gov.br>
2025-09-23 09:40:15 -0300

    terminado aula 05

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 
-   const botaoMostrapalavras = document.querySelector( "#botao-palavrachave");
+const botaoMostrapalavras = document.querySelector("#botao-palavrachave");
 
 botaoMostrapalavras.addEventListener("click", mostraPalavrasChave);
 
-function mostraPalavrasChave(){ 
-    const texto = document.querySelector("#entrada-de-texto").value;
-    
-    const campoResultado = document.querySelector("#resultado-palavrachave");
+function mostraPalavrasChave() {
+   const texto = document.querySelector("#entrada-de-texto").value;
+   campoResultado = document.querySelector("#resultado-palavrachave");
+   const palavrasChave = processaTexto(texto);
 
-    const palavrasChave = processaTexto(texto);
-    
-     campoResultado. textContent = palavras;  
+   campoResultado.textContent = palavras;
 }
-function processaTexto (texto) {
-   let palavras = texto.split(/\P{L}+/u);
+function processaTexto(texto) {
+     let palavras = texto.split(/\P{L}+/u);
+
+     let frequencias = {};
+     for ( let i of palavras) (
+      frequencia [i] = 0;
+      for (let j of palavras) {
+         if ( i ==j) {
+            frequencia [i]++;
+         }
+      }
+     )
    return palavras;
 }
