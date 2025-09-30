7fe9f4e3fd5a5f0cfadd43c57620cef74c7cf020
kaique2025 <kaique.muniz.lopes@escola.pr.gov.br>
2025-09-30 09:36:28 -0300

    aula 06

--- script.js
+++ script.js
@@ -12,15 +12,27 @@ function mostraPalavrasChave() {
 }
 function processaTexto(texto) {
      let palavras = texto.split(/\P{L}+/u);
+     const frequencia = contaFrequencias (palavras);
+     let ordenadas = Object.keys(frequencia).sort(ordenaPalavra);
 
-     let frequencias = {};
-     for ( let i of palavras) (
-      frequencia [i] = 0;
-      for (let j of palavras) {
-         if ( i ==j) {
-            frequencia [i]++;
+     function ordenaPalavra(p1,p2){
+      return frequencias [p2] - frequencias[p1];
+     }
+
+     console.log(ordenadas);
+     return ordenadas.slice(0, 10);
+   }
+   function contaFrequencias(palavras) {
+      let frequencias = {;
+         for (let i of palavras) {
+            frequencias[i] = 0;
+            for (let j of palavras) {
+               if (i== j) {
+                  frequencias[i]++;
+            }
          }
       }
-     )
    return palavras;
+
 }
+     
